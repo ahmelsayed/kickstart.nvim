df23e875addd300064348ffad23c90aa8982610c
Ahmed ElSayed <ahmels@microsoft.com>
2025-08-13 10:07:01 -0700

    fix Windows Terminal cursor

--- init.lua
+++ init.lua
@@ -219,6 +219,13 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+vim.api.nvim_create_autocmd('VimLeave', {
+  pattern = '*',
+  callback = function()
+    vim.opt.guicursor = 'a:ver30-Cursor-blinkwait700-blinkon400-blinkoff250'
+  end,
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
